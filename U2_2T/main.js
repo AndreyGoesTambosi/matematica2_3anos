305393d1d968ab953a743c0c2186c1f83773a6af
AndreyGoesTambosi <143732510+AndreyGoesTambosi@users.noreply.github.com>
2025-07-23 14:24:35 -0300

    Update main.js

--- U2_2T/main.js
+++ U2_2T/main.js
@@ -6,8 +6,20 @@ const botoes = document.querySelectorAll('.parametro-senha__botao');
 console.log(botoes);
 
 botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){ 
-tamanhoSenha = tamanhoSenha -1;
+ if (tamanhoSenha > 1){
+  //tamanhoSenha = tamanhoSenha -1;
+  tamanhoSenha--;
+}
 numeroSenha.textContent = tamanhoSenha
+}
+
+function aumentaTamanho(){
+ if (tamanhoSenha < 30){
+  //tamanhoSenha = tamanhoSenha +1;
+  tamanhoSenha++;
+}
+numeroSenha.textContent = tamanhoSenha;
 }
