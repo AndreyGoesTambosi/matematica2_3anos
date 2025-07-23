bc1d3a20fde61c59a442f25285cd7200320cf388
AndreyGoesTambosi <143732510+AndreyGoesTambosi@users.noreply.github.com>
2025-07-23 14:42:40 -0300

    Update main.js

--- U2_2T/main.js
+++ U2_2T/main.js
@@ -27,6 +27,7 @@ numeroSenha.textContent = tamanhoSenha;
 const campoSenha = document.querySelector('#campo-senha');
 
 const letrasMaiusculas = "ABCDEFGHIJKLMNOPQRSTUVXYWZ";
+geraSenha();
 
 function geraSenha(){
 let numeroAleatorio = Math.random()*letrasMaiusculas.length;
