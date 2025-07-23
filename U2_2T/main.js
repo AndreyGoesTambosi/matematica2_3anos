0937cc941e0b72fa5c7d9d2204109dc4937e7dd2
AndreyGoesTambosi <143732510+AndreyGoesTambosi@users.noreply.github.com>
2025-07-23 13:58:54 -0300

    Update main.js

--- U2_2T/main.js
+++ U2_2T/main.js
@@ -1,1 +1,6 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
 
+numeroSenha.textContent = 5;
+
+const botoes = document.querySelector('.parametro-senha__botao');
+console.log(botoes);
